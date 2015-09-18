de7b4f0073eedabd54c67ca8dfd2bfa46672cae7
nieznanysprawiciel <nieznany.sprawiciel@gmail.com>
2015-09-18 19:23:19 +0200

    Projekt EntryPoint usuniety; Dodany projekt TestEngine; EngineCore kompiluje się już tylko do liba.

--- SWEngineInclude.h
+++ SWEngineInclude.h
@@ -2,5 +2,5 @@
 
 #include "EngineCore/GamePlay/GamePlay.h"
 #include "EngineCore/MainEngine/EngineInterface.h"
-#include "basic_interfaces.h"
-#include "EngineCore\EventsManager\Event.h"
+#include "EngineCore/Actors/ActorObjects.h"
+#include "EngineCore/EventsManager/Event.h"
