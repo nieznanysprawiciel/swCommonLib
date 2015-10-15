927f58c179cb31dc76eb614d8b285c259d95ee19
nieznanysprawiciel <nieznany.sprawiciel@gmail.com>
2015-10-15 23:15:29 +0200

    Dodana klasa SpinLock oraz kilka funkcji w EngineInterface.

---
+++ Multithreading/SpinLock.h
@@ -0,0 +1,83 @@
+#include "Common/TypesDefinitions.h"
+
+/**
+@defgroup Multithreading Narzêdzia pomocnicze do obs³ugi wielow¹tkowoœci.
+@ingroup CommonFiles
+*/
+
+/**
+@brief Klasa s³u¿¹ca jako mutex z aktywnym oczekiwaniem.
+
+Klasa SpinLock u¿ywa aktywnego oczekiwania w celu unikniêcia koniecznoœci
+odwo³ywania siê do systemu operacyjnego, gdy wejœcie do sekcji krytycznej nie jest mo¿liwe.
+Implementacj¹ opiera siê o instrukcjê TSL.
+
+Mo¿liwe jest u¿ywanie klasy we wspó³pracy z mechanizmami takimi jak std::lock_guard.
+
+Ze wzglêdu na to, ¿e Visual Studio nie potrafi kompilowaæ asemblera pod architekturê ARM,
+na razie klasa nadaje siê do u¿ytku jedynie pod procesorami x86.
+*/
+class SpinLock
+{
+private:
+	volatile int16		m_locked;
+protected:
+public:
+	SpinLock()
+	{
+		m_locked = 0;
+	}
+	~SpinLock() = default;
+
+	inline void lock();
+	inline void unlock();
+	inline bool try_lock();
+};
+
+
+/**@brief Wejœcie do sekcji krytycznej.
+*/
+inline void SpinLock::lock()
+{
+	_asm
+	{
+		mov			ecx,			[this]
+	TRY_LOCK_LABEL:
+		mov			AX,				0xF
+		xchg		AX,				[ecx].m_locked
+		cmp			AX,				0x0
+		jne			TRY_LOCK_LABEL
+	}
+}
+
+
+/**@brief Opuszczenie sekcji krytycznej.
+*/
+inline void SpinLock::unlock()
+{
+	_asm mov		ecx,		[this]
+	_asm mov		EAX,		0x0
+	_asm xchg		AX,			[ecx].m_locked
+}
+
+
+/**@brief Próbuje zaj¹æ sekcjê krytyczn¹. Je¿eli jest zajêta, wychodzi natychmiast.
+
+@return Zwraca true, je¿eli uda³o siê wejœæ do sekcji krytycznej.
+*/
+inline bool SpinLock::try_lock()
+{
+	bool success = false;
+	_asm
+	{
+		mov			ecx,			[this]
+		mov			AX,				0xF
+		xchg		AX,				[ecx].m_locked
+		cmp			AX,				0x0
+		jne			LOCK_FAILED_LABEL
+	}
+	return true;
+	LOCK_FAILED_LABEL:
+	return false;
+}
+
