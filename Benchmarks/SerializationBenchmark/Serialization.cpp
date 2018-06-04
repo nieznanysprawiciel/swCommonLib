2f44075639327a87a38752ea4934f6197fe1b546
nieznanysprawiciel <nieznany.sprawiciel@gmail.com>
2018-06-04 22:40:29 +0200

    Internal serialization benchmark

--- Benchmarks/SerializationBenchmark/Serialization.cpp
+++ Benchmarks/SerializationBenchmark/Serialization.cpp
@@ -4,12 +4,13 @@
 #include "swCommonLib/TestUtils/TestClassHierarchy/SerializationPrimitives/StringContainer.h"
 
 #include "swCommonLib/Serialization/PropertySerialization/Serialization.h"
+#include "swCommonLib/Serialization/PropertySerialization/SerializationContext.h"
 
 
 
 // ================================ //
 //
-static void		Serialization	( benchmark::State& state )
+static void		SerializeAndSaveFile	( benchmark::State& state )
 {
 	sw::Node root;
 	root.GenerateTree( 10000, 20, TypeID::get< sw::StringContainer >() );
@@ -22,4 +23,25 @@ static void		Serialization	( benchmark::State& state )
 }
 
 
-BENCHMARK( Serialization )->Unit( benchmark::TimeUnit::kMillisecond )->Repetitions( 20 )->Iterations( 1 );
+BENCHMARK( SerializeAndSaveFile )->Unit( benchmark::TimeUnit::kMillisecond )->Repetitions( 20 )->Iterations( 1 );
+
+
+
+// ================================ //
+//
+static void		SerializeInternal	( benchmark::State& state )
+{
+	sw::Node root;
+	root.GenerateTree( 10000, 20, TypeID::get< sw::StringContainer >() );
+
+	for( auto _ : state )
+	{
+		sw::Serialization serial;
+		ISerializer ser( std::static_pointer_cast< ISerializationContext >( std::make_shared< sw::SerializationContext >() ) );
+		serial.Serialize( ser, root );
+	}
+}
+
+BENCHMARK( SerializeInternal )->Unit( benchmark::TimeUnit::kMillisecond )->Repetitions( 20 )->Iterations( 1 );
+
+
