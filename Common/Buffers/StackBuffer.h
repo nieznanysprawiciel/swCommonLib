ef4cc7ce836f2da6d82dfd4ba64a60909a26ac94
nieznanysprawiciel <nieznany.sprawiciel@gmail.com>
2018-12-01 22:06:47 +0100

    StackBuffer GetData fix

--- Common/Buffers/StackBuffer.h
+++ Common/Buffers/StackBuffer.h
@@ -17,11 +17,12 @@ This class wrappes structure in buffer interface.*/
 template< typename StructType >
 class StackBuffer : public StructType
 {
+	typedef StackBuffer< StructType >* ThisType;
 private:
 public:
 
 	Size			GetSize		() const		{ return sizeof( StructType ); }
-	uint8*			GetData		() const		{ return this; }
+	uint8*			GetData		() const		{ return reinterpret_cast< uint8* >( const_cast< ThisType >( this ) ); }
 
 	BufferRange		GetView		() const		{ return BufferRange( GetData(), GetSize() ); }
 };
